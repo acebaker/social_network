1def6bde180d1f5304899bf4658898e780f668fe
Jeremy Juel <jeremy.juel@gmail.com>
2011-04-09 12:17:56 -0700

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -55,4 +55,6 @@ SocialNetwork::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  map.connect ':controller/:action/:id.:format'
+  map.connect ':controller/:action/:id'
 end
